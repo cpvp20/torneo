326c7bb757fe2c3e9bcd7774e2587060930e7288
Carolina Perez-Vargas <cpvp20@gmail.com>
2021-04-29 08:52:38 -0500

    final

--- index.js
+++ index.js
@@ -2,35 +2,36 @@ const express = require('express');
 const multerS3 = require('multer-s3');
 var multer = require('multer');
 const aws = require('aws-sdk');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var s3 = new aws.S3({
-  /* ... */ })
-
-const path = require('path');
+  accessKeyId: 'AKIAYGM6GOCDJC7H2LNL',
+  secretAccessKey: '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'
+})
 
 const app = express();
 app.use(express.json());
-
 app.use(express.static(__dirname + '/'));
 
 var upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'torneo-bucket',
+    bucket: 'torneo-bucket-1',
+    acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, {
         fieldName: file.fieldname
       });
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString())
+      cb(null, Date.now().toString() + '.' + file.originalname.split('.').pop()); //date + extension
     }
   })
-})
+});
 
-app.post('/upload', upload.array('photos', 3), function (req, res, next) {
-  res.send('Successfully uploaded ' + req.files.length + ' files!')
+app.post('/upload', upload.single('profilePic'), function (req, res, next) {
+  console.log('Successfully uploaded file :)');
+  res.send('Successfully uploaded file :)')
 });
 
 app.listen(port, () => {
